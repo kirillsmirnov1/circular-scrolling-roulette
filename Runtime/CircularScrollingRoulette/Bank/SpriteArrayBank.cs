d21b3d274d8052c6eb41f6ef0676d85b5eef7c11
Kirill Smirnov <kirill@dg-line.ru>
2020-11-25 12:18:53 +0300

    Add RouletteEntryImageContent.cs and SpriteArrayBank.cs

---
+++ Runtime/CircularScrollingRoulette/Bank/SpriteArrayBank.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CircularScrollingRoulette.Bank
+{
+    public class SpriteArrayBank : RouletteBank
+    {
+#pragma warning disable 0649
+        [SerializeField] private Sprite[] sprites;
+#pragma warning restore 0649
+        
+        public override object GetRouletteContent(int index) 
+            => sprites[index];
+
+        public override int GetRouletteLength() 
+            => sprites.Length;
+    }
+}
---
+++ Runtime/CircularScrollingRoulette/Entry/Content/RouletteEntryImageContent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CircularScrollingRoulette.Entry.Content
+{
+    [RequireComponent(typeof(Image))]
+    public class RouletteEntryImageContent : RouletteEntryContent
+    {
+        private Image _image;
+
+        private void Awake() => _image = GetComponent<Image>();
+
+        public override void SetContent(object content) 
+            => _image.sprite = (Sprite) content;
+
+        public override object GetContent() 
+            => _image.sprite;
+    }
+}
